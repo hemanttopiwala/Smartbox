fc27118d6aff4deb95d5ccfd7d8561a641a5e6ff
Hemant Topiwala <hemanttopiwala212@gmail.com>
2022-05-02 00:54:07 +0530

    Add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,23 @@
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">SmartBox</a>
-      </header>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">SmartBox</Link>
+        </header>
 
-      <main>
-        <h1>Featured Products</h1>
-
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <img src={product.Image} alt={product.name} />
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>{product.price}</strong>
-                </p>
-
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
